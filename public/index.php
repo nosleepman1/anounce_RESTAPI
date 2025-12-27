27cee7ba256201d207e6d444bdbe21af14551f65
nosleepman1 <abdallahdiouf.dev@gmail.com>
2025-12-27 20:26:42 +0000

    refacto #2

--- public/index.php
+++ public/index.php
@@ -2,5 +2,28 @@
 
 define('BASE_PATH', dirname(__DIR__));
 
-require_once BASE_PATH . '/src/config/db.php';
-require_once BASE_PATH . '/src/routes/user.routes.php';
+$request = $_SERVER['REQUEST_URI'];
+$scriptname = $_SERVER['SCRIPT_NAME'];
+
+$url = str_replace($scriptname, '', $request);
+$url = trim($url,'/');
+$url = strtok($url, '?');
+
+
+
+
+switch ($url) {
+    case '':
+    case 'accueil':
+        echo 'PAGE ACCUEIL';
+        break;
+    case 'contact':
+        echo 'PAGE CONTACT';
+        break;
+    case 'about':
+        echo 'PAGE ABOUT';
+        break;
+    default:
+        echo 'PAGE NON TROUVEE';
+        break;
+}
--- src/config/db.php
+++ src/config/db.php
@@ -1,28 +1,12 @@
 <?php 
     class dataBase {
-        private static $instance = null;
-        private $conn;
-        private $dbname= "annonce_api";
-        private $username = 'root';
-        private $pwd = '';
-        private $server = 'localhost';
+        private static ?PDO $instance = null;
 
-        private function __construct() {
-            try {
-                $this->conn = new PDO("mysql:host={$this->server};dbname={$this->dbname}", $this->username, $this->pwd);
-                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            } catch (PDOException $e) {
-                http_response_code(500);
-                echo json_encode(['success' => false, 'message' => $e->getMessage()]);
-            }
-        }
+        private function __construct() {}
 
-        public static function getInstance (){
-            if (!self::$instance) {
-                self::$instance = new dataBase();
-                return self::$instance->conn;
-            }
-        }
+        public static function getInstance(): PDO {
+            if (self::$instance === null) {
+                
     }
 
 ?>
